a96df332b0e3fbe71ca957b86ea25d51b801b6d9
Matty-Montoya <mmontoya92@gmail.com>
2018-05-30 13:52:08 -0400

    Continuing to work on the parts template
    -Still have yet to figure out how to target those parts through the collection
    handlebars.
    -Created a for loop to check for the parts.

--- assets/scripts/collections/ui.js
+++ assets/scripts/collections/ui.js
@@ -3,6 +3,12 @@
 const showCollectionTemplate = require('../templates/collections.handlebars')
 
 const getCollectionsSuccess = function (data) {
+  console.log(data.collections)
+  for (let i = 0; i < data.collections.length; i++) {
+    if (data.collections[i].collection_name === 'Alien Workshop') {
+      console.log(data.collections[i].parts)
+    }
+  }
   const showCollectionHTML = showCollectionTemplate({collections: data.collections})
   $('.collection-content').html(showCollectionHTML)
 }
--- assets/scripts/parts/ui.js
+++ assets/scripts/parts/ui.js
@@ -1,7 +1,6 @@
 const showPartTemplate = require('../templates/part.handlebars')
 
 const getPartsSuccess = function (data) {
-  console.log('data is', data)
   const showPartHTML = showPartTemplate({parts: data.parts})
   $('.part-content').html(showPartHTML)
 }
